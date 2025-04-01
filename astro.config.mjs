b8b6183cb2f61e131fc51c07d15e6acf08d5b802
JokerWon <1595672103@qq.com>
2025-04-01 17:21:58 +0800

    feat: import unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 // @ts-check
 import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
 })
